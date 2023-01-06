31f6aade2a5376ad2c88a861f9eedf319762739c
oGabrielDev <gabriel.thiago611@gmail.com>
2023-01-06 01:51:29 -0300

    fix: Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Contato from './pages/Contato';
 import Home from './pages/Home';
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <Switch>
+      <Redirect from="/Portfolio" to="/" />
       <Route exact path="/" component={Home} />
       <Route exact path="/contato" component={Contato} />
     </Switch>
