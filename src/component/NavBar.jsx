3400b18e0c1e5be2c981ebd5f4c26a1b9eade574
oGabrielDev <gabriel.thiago611@gmail.com>
2023-01-07 00:38:14 -0300

    Fix: Update Routes

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function navbar() {
   return (
     <ul className="nav justify-content-end">
       <li className="nav-item">
-        <a className="nav-link" href="/">Home</a>
+        <Link className="nav-link" to="/">Home</Link>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/contato">Contato</a>
+        <Link className="nav-link" to="/contato">Contato</Link>
       </li>
     </ul>
   );
