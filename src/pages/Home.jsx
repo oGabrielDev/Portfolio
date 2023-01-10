b058f595b5c8d4c53a44c88ba2506c996a41da19
oGabrielDev <gabriel.thiago611@gmail.com>
2023-01-10 00:14:24 -0300

    Fix: Erro Deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import NavBar from '../component/NavBar';
 import Footer from '../component/Footer';
 import Header from '../component/Header';
-import Main from '../component/Main';
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Header />
-      <Main />
       <Footer />
     </>
   );
