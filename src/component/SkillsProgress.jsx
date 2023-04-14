e0bc0a7ec9cebf2cd83a60a687dabea67e7b94db
oGabrielDev <gabriel.thiago611@gmail.com>
2023-04-14 17:57:36 -0300

    feat: Adicionado as Skills e o CSS

--- src/component/SkillsProgress.jsx
+++ src/component/SkillsProgress.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
+import '../Styles/SkillsProgress.css';
 
 export default function SkilsProgress() {
   return (
-    <div className="ContainerSkills">
-      <div>
+    <div className="containerSkills">
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=js" alt="logoJS" />
         <p>JavaScript</p>
       </div>
-      <div>
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=nodejs" alt="logoNode" />
         <p>Node.JS</p>
       </div>
-      <div>
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=ts" alt="logoTS" />
         <p>TypeScript</p>
       </div>
-      <div>
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=mysql" alt="logoMysql" />
         <p>MySql</p>
       </div>
-      <div>
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=mongodb" alt="logoMongo" />
         <p>MongoDB</p>
       </div>
-      <div>
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=py" alt="logoMongo" />
         <p>Python</p>
       </div>
-      <div>
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=docker" alt="logoMongo" />
         <p>Docker</p>
       </div>
-      <div>
+      <div className="skills">
         <img src="https://skillicons.dev/icons?i=react" alt="logoMongo" />
         <p>React</p>
       </div>
